1ef05a6ddeadda1c3e3ac2a96e6503012a90d47a
rinti <hello@rinti.se>
2017-06-23 16:10:25 +0200

    Move state to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import orderBy from 'lodash/orderBy'
 import List from './List'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    let itemsPromise = fetch('http://localhost:3333/items/').then(item => item.json())
+
+    this.state = {items: [], ordering: 'asc'}
+
+    itemsPromise.then((items) => {
+      this.setState({
+        items: orderBy(items, 'seconds', this.state.ordering)
+      })
+    })
+
+    this.switchOrdering = this.switchOrdering.bind(this)
+  }
+
+  switchOrdering() {
+    this.setState((prevState) => {
+      const newOrder = prevState.ordering === 'asc' ? 'desc' : 'asc'
+      return {
+        ordering: newOrder,
+        items: orderBy(prevState.items, 'seconds', newOrder)
+      }
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -12,7 +39,8 @@ class App extends Component {
         <div className="App-list">
           <div className="List-wrapper">
             <div className="List-half">
-              <List />
+              <button onClick={this.switchOrdering}>Reverse order</button>
+              <List items={this.state.items} />
             </div>
             <div className="List-half">
               <div className="Selected-item">
